3b5fbf49a23fd447f107af2ded42094c9775070f
Nishant <nishantminerva@gmail.com>
2024-04-01 07:43:04 +0530

    Initial Commit

---
+++ VivekApp/VivekAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VivekAppApp.swift
+//  VivekApp
+//
+//  Created by Nishant Minerva on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct VivekAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
